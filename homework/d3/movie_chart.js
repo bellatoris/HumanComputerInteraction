b737da9efec42ca70f2871d4133d1590565bd2a9
bellatoris <bellatoris@snu.ac.kr>
2017-05-12 22:26:57 +0900

    [HCI] 요약 과제 완료…

---
+++ homework/d3/movie_chart.js
@@ -0,0 +1,13 @@
+function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(init);
+
+function init() {
+    var svg = d3.select('svg');
+}
